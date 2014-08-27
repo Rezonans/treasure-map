6f9cad5e31118e7cac16a93486d80ce247a84f17
Rodik <i6@bk.ru>
2014-08-27 15:26:38 +0300

    index and destroy actions for the fields controller

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -1,8 +1,20 @@
 class FieldsController < ApplicationController
   def create
-    @field = Field.new params.require(:field).permit([:cells])
+    @field = Field.new params.require(:field).permit(:cells)
     if @field.save
       redirect_to '/'
     end
+  end
+
+  def index
+    @fields = Field.last(5)
+
+    render json: @fields.map { |field| {cells: field.cells, url: moves_url, field_id: field.id} }
+  end
+
+  def destroy
+    Field.destroy(params[:id])
+
+    render json: {status: :ok}
   end
 end
