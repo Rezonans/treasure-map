78a1dd411733e414f74185b659688e331855040c
Rodik <i6@bk.ru>
2014-08-27 15:24:09 +0300

    little refactoring for permit_params

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -1,9 +1,13 @@
 class MovesController < ApplicationController
   def create
-    data = params.require(:move).permit([:direction, :client_id, :field_id])
-
-    cell = Position.move(data[:client_id], data[:field_id], data[:direction])
+    cell = Position.move(permit_params[:client_id], permit_params[:field_id], permit_params[:direction])
 
     render json: {status: :ok, field: cell.name}
+  end
+
+  protected
+
+  def permit_params
+    @permit_params ||= params.require(:move).permit([:direction, :client_id, :field_id])
   end
 end
